45a4f753a069ed2413932f61959b7df59c393449
CallmeHenry <henry_nguyen777@hotmail.com>
2019-06-24 05:05:57 -0700

    initial commit

---
+++ js/type.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typing', {
+    strings: ["Hello! ^500", "My name is Henry Nguyen.^750", "Sorry but my website is currently under construction.^500", "Take a look at my LinkedIn or Github for my credentials!"],
+    typeSpeed: 20,
+    startDelay: 500,
+    backSpeed: 25,
+    showCursor: false
+
+})
